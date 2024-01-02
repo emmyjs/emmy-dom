320f2f5612b5745ba229f8381af7dbdf8e78b44c
Emmanuel Norambuena <eanorambuena@uc.cl>
2024-01-02 18:19:41 -0300

    chore(Root): Add lint script

--- dist/ssr/ssr/register/dom/HTMLElement.js
+++
@@ -1,5 +0,0 @@
-const Element = require('./Element');
-
-// Most nodes should derive from this but they derive from Element instead,
-// so we've put everything there and just expose this as a synonnym.
-module.exports = Element;
--- dist/ssr/ssr/register/dom/__tests__/Object.js
+++
@@ -1,4 +0,0 @@
-// Still unsure why this is failing.
-test.skip('should still be instanceof object', () => {
-  expect({}).toBeInstanceOf(Object);
-});
